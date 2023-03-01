c0248d7f4a2f5506c488b35a05e06026cef71910
visakhcv <visakhvisa12345@gmail.com>
2023-03-01 19:43:20 +0530

    v1

---
+++ script.js
@@ -0,0 +1,15 @@
+let year=6
+let body=5
+
+if (year >= 5){
+    if(body='m'){
+        console.log('male');
+    }
+    else if(body='f'){
+        console.log('female');
+    }
+     
+}
+else{
+    console.log('not eligible');
+}
